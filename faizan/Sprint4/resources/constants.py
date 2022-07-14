ab838fb7d28529d23d2429f1708f98254abf0348
muhammadfaizan2022skipq <muhammadfaizan.ikram.skipq@gmail.com>
2022-07-14 18:18:59 +0500

    Updated Sprint4 files - fixed errors

--- faizan/Sprint4/resources/constants.py
+++ faizan/Sprint4/resources/constants.py
@@ -2,7 +2,6 @@
 
 MY_URLS_VAR = []
 
-URL_TO_MONITOR = ["skipq.org", "leetcode.com", "github.com", "namal.edu.pk"]
 URL_MONITOR_NAMESPACE = "FaizanAWS"
 URL_MONITOR_NAME_AVAILABILITY = "url_availability"
 URL_MONITOR_NAME_LATENCY = "url_latency"
